81c9646e13ee8981833dae1146e49e06260e4f6e
OyakiMorse <legenda1200@gmail.com>
2021-03-05 12:16:53 +0200

    Improved header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,3 +1,21 @@
 <template>
-  <header>Hello Dudes Corp.</header>
+  <header>
+    <p>Royal ToDo</p>
+    <div class="header__inner">
+      <ul class="list">
+        <li class="item">
+          <a href="#" class="link">Tasks</a>
+        </li>
+        <li class="item">
+          <a href="#" class="link">Saved</a>
+        </li>
+        <li class="item">
+          <a href="#" class="link">Completed</a>
+        </li>
+        <li class="item">
+          <a href="#" class="link">Completed</a>
+        </li>
+      </ul>
+    </div>
+  </header>
 </template>
