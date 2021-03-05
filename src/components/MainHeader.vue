12cac7de11aa42f5fc070bfd0c32cae7fa3ab139
OyakiMorse <legenda1200@gmail.com>
2021-03-05 12:16:53 +0200

    Improved header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <h1>Hello world</h1>
-  </div>
+  <header>
+    <p>Royal ToDo</p>
+    <div class="header__inner">
+      <ul class="list">
+        <li class="item">
+          <a href="#" class="link">Tasks</a>
+        </li>
+        <li class="item">
+          <a href="#" class="link">Saved</a>
+        </li>
+        <li class="item">
+          <a href="#" class="link">Completed</a>
+        </li>
+      </ul>
+    </div>
+  </header>
 </template>
