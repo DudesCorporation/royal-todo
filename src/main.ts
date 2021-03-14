46a755f93865096f55834a3f9364a9afc0905f5a
Nazarii <puffz405@gmail.com>
2021-03-14 13:53:43 +0200

    divide drawer and header

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Icon from '@/components/Icon.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'tailwindcss/dist/tailwind.min.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+app.component('Icon', Icon);
+
+app.use(store).use(router).mount('#app')
