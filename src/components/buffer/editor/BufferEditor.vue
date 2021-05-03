28487b2b64a34ed6e23de141aaf6eafc8bf05c05
Mark Yarchak <markyarchak.work@gmail.com>
2021-05-04 00:38:06 +0300

    feat: add buffer editor modes

--- src/components/buffer/editor/BufferEditor.vue
+++ src/components/buffer/editor/BufferEditor.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex flex-col flex-grow-2">
     <BufferEditorHeader />
+    <BufferEditorModes />
     <BufferEditorContent />
   </div>
 </template>
@@ -8,12 +9,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BufferEditorHeader from '@/components/buffer/editor/BufferEditorHeader.vue';
+import BufferEditorModes from '@/components/buffer/editor/BufferEditorModes.vue';
 import BufferEditorContent from '@/components/buffer/editor/BufferEditorContent.vue';
 
 export default defineComponent({
   name: 'BufferEditor',
   components: {
     BufferEditorHeader,
+    BufferEditorModes,
     BufferEditorContent,
   },
 });
---
+++ src/components/buffer/editor/BufferEditorModes.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex bg-gray-100 border-b border-gray-300">
+    <button
+      v-for="(mode, index) in editorModes"
+      :key="index"
+      class="editor-mode"
+      :class="{ 'active': mode === active }"
+      @click="setActive(mode)"
+    >
+      {{ mode }}
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'BufferEditorModes',
+  setup() {
+    const editorModes = ['plain', 'rich', 'code', 'any'];
+    const active = ref('plain');
+    const setActive = newActive => {
+      active.value = newActive;
+    };
+
+    return {
+      editorModes,
+      active,
+      setActive,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+  .editor-mode {
+    @apply focus:outline-none flex-grow w-1/4;
+
+    &.active {
+      @apply bg-gray-300;
+    }
+  }
+</style>
