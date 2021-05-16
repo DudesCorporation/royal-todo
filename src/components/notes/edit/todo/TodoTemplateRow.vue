23824afc1abcdc08937f3dce72bcc770b21ad96c
Mark Yarchak <markyarchak.work@gmail.com>
2021-05-16 22:10:34 +0300

    feat: design draggable template columns

--- src/components/notes/edit/todo/TodoTemplateRow.vue
+++ src/components/notes/edit/todo/TodoTemplateRow.vue
@@ -11,28 +11,45 @@
       />
     </TodoColumn>
 
-    <TodoColumn class="flex items-center font-medium px-3 py-2">
-      <div>Title</div>
-    </TodoColumn>
+    <DraggableList
+      class="flex flex-1"
+      :items="columns"
+    >
+      <template #item="props">
+        <TodoColumn
+          v-bind="props"
+          class="flex items-center font-medium px-3 py-2"
+        >
+          <div>{{ props.item }}</div>
+        </TodoColumn>
+      </template>
+    </DraggableList>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TodoColumn from '@/components/notes/edit/todo/TodoColumn.vue';
+import DraggableList from '@/components/drag-and-drop/DraggableList.vue';
 import IconBtn from '@/components/root/IconBtn.vue';
 
 export default defineComponent({
   name: 'TodoTemplateRow',
   components: {
     TodoColumn,
+    DraggableList,
     IconBtn,
   },
   props: {
-    columns: {
-      type: Array,
-      required: true,
-    },
+    // columns: {
+    //   type: Array,
+    //   required: true,
+    // },
+  },
+  setup() {
+    return {
+      columns: ['Title', 'Title', 'Title'],
+    };
   },
 });
 </script>
