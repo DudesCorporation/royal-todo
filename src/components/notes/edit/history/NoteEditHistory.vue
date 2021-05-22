246585fa68df4f119f19b5934eb314186fca1f87
Mark Yarchak <markyarchak.work@gmail.com>
2021-05-23 01:41:19 +0300

    feat: add note history section

---
+++ src/components/notes/edit/history/NoteEditHistory.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="NoteEditHistory">
+    <div class="text-lg font-bold">Note history</div>
+    <div>hello</div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NoteEditHistory',
+});
+</script>
+
+<style lang="scss" scoped>
+  .NoteEditHistory {
+    @apply h-full w-full bg-white overflow-y-auto;
+  }
+</style>
