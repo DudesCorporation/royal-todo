c4215388ff21df67ce8900e9bfaa4d2530b4c79c
Mark Yarchak <markyarchak.work@gmail.com>
2021-05-23 16:22:10 +0300

    feat (layout): add note history actions

--- src/components/notes/edit/history/NoteEditHistory.vue
+++ src/components/notes/edit/history/NoteEditHistory.vue
@@ -1,30 +1,33 @@
 <template>
   <section class="NoteEditHistory">
-    <div class="ml-2 my-1 text-lg font-bold">Note history</div>
-
-    <div class="px-1">
+    <div class="flex-1 px-1 overflow-y-auto">
+      <div class="mx-2 my-1 text-lg font-bold">Note history</div>
       <NoteHistoryItem
-        v-for="item in 5"
+        v-for="item in 105"
         :key="item"
       />
     </div>
+
+    <NoteHistoryActions class="sticky bottom-0" />
   </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import NoteHistoryItem from '@/components/notes/edit/history/NoteHistoryItem.vue';
+import NoteHistoryActions from '@/components/notes/edit/history/NoteHistoryActions.vue';
 
 export default defineComponent({
   name: 'NoteEditHistory',
   components: {
     NoteHistoryItem,
+    NoteHistoryActions,
   },
 });
 </script>
 
 <style lang="scss" scoped>
   .NoteEditHistory {
-    @apply h-full w-full bg-white overflow-y-auto;
+    @apply flex flex-col h-full w-full bg-white;
   }
 </style>
