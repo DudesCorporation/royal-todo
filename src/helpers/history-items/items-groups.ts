13a0a74ad2839315a969465eb0b839c48c7ebf61
Mark Yarchak <markyarchak.work@gmail.com>
2021-06-08 00:52:55 +0300

    feat: add relative history item time, remove redundant comments

--- src/helpers/history-items/items-groups.ts
+++ src/helpers/history-items/items-groups.ts
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon';
 import type { HistoryItem } from '@/models/HistoryItem';
 
 export interface HistoryItemsGroup {
@@ -6,15 +7,28 @@ export interface HistoryItemsGroup {
 }
 
 const historyTitlesLists = {
-  notes: [
-    'Recent changes',
-  ],
+  notes(group: HistoryItemsGroup, index: number) {
+    // TODO temporary check, add isOpenSession instead
+    if (!index) return 'Recent changes';
+
+    return formatFromTimestamp(group.items[group.items.length - 1].eventDate);
+  },
 };
 
+function formatFromTimestamp(timestamp: number) {
+  return DateTime.fromMillis(timestamp).toRelative();
+}
+
+function getHistoryGroupTitle(group: HistoryItemsGroup, index: number) {
+  return historyTitlesLists.notes(group, index);
+}
+
+function applyHistoryGroupsTitle(group: HistoryItemsGroup, index: number) {
+  const title = getHistoryGroupTitle(group, index);
+
+  Object.assign(group, { title });
+}
+
 export function applyHistoryGroupsTitles(itemsGroups: Array<HistoryItemsGroup>) {
-  itemsGroups.forEach((g, idx) => {
-    Object.assign(g, {
-      title: historyTitlesLists.notes[idx] || g.items[g.items.length - 1].eventDate,
-    });
-  });
+  itemsGroups.forEach(applyHistoryGroupsTitle);
 }
